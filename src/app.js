4b466b80d8639322da5255fae62516ea3804c699
swolekat <swolecattwitch@gmail.com>
2022-10-24 23:43:15 -0500

    remove comment.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,3 @@ import "./stylesheets/main.css";
 import './paths';
 import './left-bar';
 import './right-bar';
-
-
-// // play silent audio to prevent background throttling?
-// const context = new AudioContext()
-// const source = context.createBufferSource()
-// source.connect(context.destination)
-// source.start(0)
